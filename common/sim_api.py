b4b50757cb1207ea7a5c520b4d5e22dec4367ed1
Frmissjing <892153623@qq.com>
2024-05-31 18:21:45 +0800

    add sim_api.py

---
+++ common/sim_api.py
@@ -0,0 +1,17 @@
+'''
+Author: Frmissjing 892153623@qq.com
+Date: 2024-05-31 10:34:40
+LastEditors: Frmissjing 892153623@qq.com
+LastEditTime: 2024-05-31 18:21:45
+FilePath: /ofono_test/common/sim_api.py
+Description: 
+
+Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+'''
+
+
+
+sim = {
+    
+}
+
